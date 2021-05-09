ef01b957ae9a2c3ce7ac8a86470bd45f4a729a18
sjsig <steviejoesig49@gmail.com>
2021-05-09 16:52:44 -0400

    fix

--- main.sh
+++ main.sh
@@ -4,9 +4,6 @@ input_dataset=$1
 output_dir=$2
 destination_folder='~/PAN_submission'
 
-# Unzip input data
-unzip ${input_dataset} -d ${destination_folder}
-
 # Process xml files 
-python preprocessing.py --data_dir ${destination_folder}
+python preprocessing.py --data_dir ${input_dataset}
 
