09ec5d0ce15e140cbd144ba05e69ca2ad9bebf0c
sjsig <steviejoesig49@gmail.com>
2021-05-09 17:04:44 -0400

    Pyton3

--- main.sh
+++ main.sh
@@ -5,5 +5,5 @@ output_dir=$2
 destination_folder='~/PAN_submission'
 
 # Process xml files 
-python preprocessing.py --data_dir ${input_dataset}
+python3 preprocessing.py --data_dir ${input_dataset}
 
