fc6559edbc34b36222a22ad49ef5ed1c1da37382
sjsig <steviejoesig49@gmail.com>
2021-05-09 18:16:15 -0400

    stuff

--- main.sh
+++ main.sh
@@ -15,3 +15,5 @@ python3 preprocessing.py --data_dir ${input_dataset}
 echo 'Prepro'
 python3 mt-dnn/prepro_std.py --model ${model} --root_dir ${en_dir} --task_def ${task_def}
 
+echo 'Predicting'
+python3 mt-dnn/predict.py --task_def ${task_def} --task twitter --score results/en --checkpoint models/en.pt --prep_input processed_data/en/xlnet-base-cased/twitter_test.json
