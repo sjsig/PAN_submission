cafe74f61c857467200b76d0bb4677b02f896fce
sjsig <steviejoesig49@gmail.com>
2021-05-10 09:38:22 -0400

    pls

--- script.py
+++ script.py
@@ -1,30 +1,37 @@
-from transformers import AutoTokenizer, AutoModelForSequenceClassification, XLNetConfig, XLNetModel
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, XLNetConfig, XLNetModel, AutoConfig
 import torch
 import json
 import os
+from mt_dnn.model import MTDNNModel
 
 model_name = "xlnet-base-cased"
-model_path = './models/hf_model_0.pt'
+model_path = './models/en.pt'
 data_path = './processed_data/en/data.json'
 
 with open(data_path) as json_file:
     data = json.load(json_file)
 
-
+# state_dict = torch.load(init_model, map_location=device)
+state_dict=None
+config = AutoConfig.from_pretrained(model_name).to_dict()
+model = MTDNNModel(config, state_dict=state_dict)
+model.load(model_path)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model.eval()
 
 # model = AutoModelForSequenceClassification.from_pretrained(model_path)
 # device = torch.device('cuda', 1)
 # state_dict = torch.load(model_path, map_location=device)
 
-print("trying", os.path.exists(model_path))
-model_state_dict = torch.load(model_path)
-print('Model dict')
-# print(model_state_dict)
+# print("trying", os.path.exists(model_path))
+# model_state_dict = torch.load(model_path)
+# print('Model dict')
+# # print(model_state_dict)
 
-model = AutoModelForSequenceClassification.from_pretrained(model_name)
+# model = AutoModelForSequenceClassification.from_pretrained(model_name)
 
-model.load_state_dict(model_state_dict["state_dict"])
-tokenizer = AutoTokenizer.from_pretrained(model_name)
+# model.load_state_dict(model_state_dict["state_dict"])
+
 
 for author in data:
     tweets = author["posts"]
