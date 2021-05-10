0b72833e1604ebd71c39e75794fe01ce37d32c35
sjsig <steviejoesig49@gmail.com>
2021-05-10 12:09:32 -0400

    pls

--- generate_output.py
+++ generate_output.py
@@ -3,6 +3,9 @@ import json
 import argparse 
 
 def generate_output(output_dir, results_dir, lang):
+    output_dir = os.path.join(output_dir, lang)
+    if(not os.path.exists(args.output_dir)):
+        os.makedirs(args.output_dir)
     
     result_file = os.path.join(results_dir, f'{lang}.json')
     with open(result_file) as json_file:
@@ -23,7 +26,6 @@ if __name__ == '__main__':
     parser.add_argument('--results_dir', type=str, default="./results")
     args = parser.parse_args()
 
-    if(not os.path.exists(args.output_dir)):
-        os.makedirs(args.output_dir)
+    
 
     generate_output(args.output_dir, args.results_dir, 'en')
