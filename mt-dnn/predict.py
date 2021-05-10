fdce44ccacddd32f88c7bb2277e0cf44241099d6
sjsig <steviejoesig49@gmail.com>
2021-05-09 20:52:06 -0400

    pls

--- mt-dnn/predict.py
+++ mt-dnn/predict.py
@@ -62,6 +62,8 @@ config['fp16'] = False
 config['answer_opt'] = 0
 config['adv_train'] = False
 del state_dict['optimizer']
+
+config = namedtuple("Config", config.keys())(*config.values())
 model = MTDNNModel(config, state_dict=state_dict)
 encoder_type = config.get('encoder_type', EncoderModelType.BERT)
 # load data
