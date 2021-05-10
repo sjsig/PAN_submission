b71942f8649115d53a2110cdc218ac14032f458c
sjsig <steviejoesig49@gmail.com>
2021-05-10 09:49:13 -0400

    pls

--- mt-dnn/predict.py
+++ mt-dnn/predict.py
@@ -73,8 +73,9 @@ del state_dict['optimizer']
 # if args.encoder_type not in EncoderModelType._value2member_map_:
 #     raise ValueError("encoder_type is out of pre-defined types")
 # literal_encoder_type = EncoderModelType(args.encoder_type).name.lower()
-# config_class, model_class, tokenizer_class = (XLNetConfig, XLNetModel, XLNetTokenizer)
-# config = config_class.from_pretrained("xlnet-base-cased").to_dict()
+config_class, model_class, tokenizer_class = (XLNetConfig, XLNetModel, XLNetTokenizer)
+xlnet_config = config_class.from_pretrained("xlnet-base-cased").to_dict()
+print(xlnet_config["num_hidden_layers"])
 
 # opt = vars(args)
 # opt.update(config)
